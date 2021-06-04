36141115bc9f6cbbfe1628b0caf81a5b0fbcb7dc
Arturo Pie <arturop@nulogy.com>
2021-06-04 10:09:21 -0400

    removes failing spec

--- spec/buildkite_watcher_spec.rb
+++ spec/buildkite_watcher_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe BuildkiteWatcher do
   it "has a version number" do
     expect(BuildkiteWatcher::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
