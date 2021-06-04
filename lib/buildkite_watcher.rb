1a4c167d853a05b1202aba489b8be95e6c5e8cea
Arturo Pie <arturop@nulogy.com>
2021-06-04 12:52:24 -0400

    makes the executable work when installed

--- lib/buildkite_watcher.rb
+++ lib/buildkite_watcher.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 require_relative "buildkite_watcher/version"
+require "buildkite_watcher/progress_bar_runner"
+require "buildkite_watcher/progress_bar"
+require "buildkite_watcher/buildkite_build_checker"
 
 module BuildkiteWatcher
   class Error < StandardError; end
