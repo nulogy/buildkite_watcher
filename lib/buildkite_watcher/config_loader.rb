3cf8f0191045d27e41ef6612a0c198b8bd4d4ccc
Arturo Pie <arturop@nulogy.com>
2021-07-16 13:16:13 -0400

    prompts the user for the pipeline slug to watch

--- lib/buildkite_watcher/config_loader.rb
+++ lib/buildkite_watcher/config_loader.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "tty-config"
+require "tty-prompt"
 require "buildkite_watcher/config"
 
 module BuildkiteWatcher
@@ -8,10 +9,10 @@ module BuildkiteWatcher
     CONFIG_FILE_NAME = ".buildkite_watcher.yml"
     SECRETS_FILE_NAME = ".buildkite_watcher_secrets.yml"
 
-    def self.load(config = TTY::Config.new, secrets = TTY::Config.new)
+    def self.load(config = TTY::Config.new, secrets = TTY::Config.new, prompt = TTY::Prompt.new)
       config.filename = CONFIG_FILE_NAME
       config.append_path Dir.pwd
-      config.read if config.exist?
+      config.exist? ? config.read : generate_config(prompt)
 
       secrets.filename = SECRETS_FILE_NAME
       secrets.append_path Dir.pwd
@@ -20,6 +21,12 @@ module BuildkiteWatcher
       secrets.read if secrets.exist?
 
       Config.new(config, secrets)
+    end
+
+    def self.generate_config(prompt)
+      prompt.ok("Welcome to Buildkite Watcher!")
+      prompt.say("I can't find my configuration file, so I'll generate one for you now.")
+      prompt.ask("What's the pipeline slug of the pipeline you want to watch?")
     end
   end
 end
--- spec/buildkite_watcher/config_loader_spec.rb
+++ spec/buildkite_watcher/config_loader_spec.rb
@@ -4,29 +4,46 @@ module BuildkiteWatcher
   RSpec.describe ConfigLoader do
     let(:config) { tty_config_double }
     let(:secrets) { tty_config_double }
+    let(:prompt) { instance_double(TTY::Prompt, say: nil, ok: nil, ask: nil) }
 
     it "reads config file if it exists" do
       allow(config).to receive(:exist?).and_return(true)
-      ConfigLoader.load(config, secrets)
+      load
       expect(config).to have_received(:read)
     end
 
     it "does not read config file if it doesn't exist" do
       allow(config).to receive(:exist?).and_return(false)
-      ConfigLoader.load(config, secrets)
+      load
       expect(config).not_to have_received(:read)
     end
 
     it "reads secrets file if it it exists" do
       allow(secrets).to receive(:exist?).and_return(true)
-      ConfigLoader.load(config, secrets)
+      load
       expect(secrets).to have_received(:read)
     end
 
     it "does not read secrets file if it doesn't exist" do
       allow(secrets).to receive(:exist?).and_return(false)
-      ConfigLoader.load(config, secrets)
+      load
       expect(secrets).not_to have_received(:read)
+    end
+
+    it "prompts the user if config file is missing" do
+      allow(config).to receive(:exist?).and_return(false)
+      allow(prompt).to receive(:ask).and_return("my-org/my-pipeline")
+
+      load
+
+      expect(prompt).to have_received(:say)
+      expect(prompt).to have_received(:ask).with(/What's the pipeline slug of the pipeline you want to watch/)
+    end
+
+    it "prompts the user if secrets file is missing"
+
+    def load
+      ConfigLoader.load(config, secrets, prompt)
     end
 
     def tty_config_double
