6d3be94ed145e3bee7cb65042bba5992bf1b5645
Arturo Pie <arturop@nulogy.com>
2021-07-16 14:43:58 -0400

    renames method for consistency

--- lib/buildkite_watcher/config_loader.rb
+++ lib/buildkite_watcher/config_loader.rb
@@ -14,7 +14,7 @@ module BuildkiteWatcher
       config.filename = CONFIG_FILE_NAME
       config.extname = EXTENSION
       config.append_path Dir.pwd
-      config.exist? ? config.read : generate_config(config, prompt)
+      config.exist? ? config.read : generate_config_file(config, prompt)
 
       secrets.filename = SECRETS_FILE_NAME
       config.extname = EXTENSION
@@ -26,7 +26,7 @@ module BuildkiteWatcher
       Config.new(config, secrets)
     end
 
-    def self.generate_config(config, prompt)
+    def self.generate_config_file(config, prompt)
       prompt.ok("Welcome to Buildkite Watcher!")
       prompt.say("I can't find my configuration file, so I'll generate one for you now.")
       pipeline_slug = prompt.ask("What's the pipeline slug of the pipeline you want to watch?")
