30d63f48428b4e899a01c055d349bc6ca86ae4de
Arturo Pie <arturop@nulogy.com>
2021-07-16 14:33:10 -0400

    tty config wants us to use extname instead of extension in filename

--- lib/buildkite_watcher/config_loader.rb
+++ lib/buildkite_watcher/config_loader.rb
@@ -6,15 +6,18 @@ require "buildkite_watcher/config"
 
 module BuildkiteWatcher
   module ConfigLoader
-    CONFIG_FILE_NAME = ".buildkite_watcher.yml"
-    SECRETS_FILE_NAME = ".buildkite_watcher_secrets.yml"
+    CONFIG_FILE_NAME = ".buildkite_watcher"
+    SECRETS_FILE_NAME = ".buildkite_watcher_secrets"
+    EXTENSION = ".yml"
 
     def self.load(config = TTY::Config.new, secrets = TTY::Config.new, prompt = TTY::Prompt.new)
       config.filename = CONFIG_FILE_NAME
+      config.extname = EXTENSION
       config.append_path Dir.pwd
-      config.exist? ? config.read : generate_config(prompt)
+      config.exist? ? config.read : generate_config(config, prompt)
 
       secrets.filename = SECRETS_FILE_NAME
+      config.extname = EXTENSION
       secrets.append_path Dir.pwd
       secrets.append_path Dir.home
 
@@ -23,10 +26,12 @@ module BuildkiteWatcher
       Config.new(config, secrets)
     end
 
-    def self.generate_config(prompt)
+    def self.generate_config(config, prompt)
       prompt.ok("Welcome to Buildkite Watcher!")
       prompt.say("I can't find my configuration file, so I'll generate one for you now.")
-      prompt.ask("What's the pipeline slug of the pipeline you want to watch?")
+      pipeline_slug = prompt.ask("What's the pipeline slug of the pipeline you want to watch?")
+      config.set(:pipeline_slug, value: pipeline_slug)
+      config.write(create: true)
     end
   end
 end
--- spec/buildkite_watcher/config_loader_spec.rb
+++ spec/buildkite_watcher/config_loader_spec.rb
@@ -2,10 +2,17 @@
 
 module BuildkiteWatcher
   RSpec.describe ConfigLoader do
-    let(:tmp_dir) { Dir.mktmpdir }
+    let(:tmp_dir) { Dir.mktmpdir(SecureRandom.uuid) }
+    let(:config_file_name) { File.join(tmp_dir, "#{ConfigLoader::CONFIG_FILE_NAME}#{ConfigLoader::EXTENSION}") }
+    let(:secrets_file_name) { File.join(tmp_dir, "#{ConfigLoader::SECRETS_FILE_NAME}#{ConfigLoader::EXTENSION}") }
     let(:config) { create_tty_config }
     let(:secrets) { create_tty_config }
-    let(:prompt) { instance_double(TTY::Prompt, say: nil, ok: nil, ask: nil) }
+    let(:prompt) { instance_double(TTY::Prompt, say: nil, ok: nil, ask: "my-response") }
+
+    before do
+      stub_const("BuildkiteWatcher::ConfigLoader::CONFIG_FILE_NAME", "buildkite_watcher_test")
+      stub_const("BuildkiteWatcher::ConfigLoader::SECRETS_FILE_NAME", "buildkite_watcher_secrets_test")
+    end
 
     it "loads pipeline_slug from config file if it exists" do
       create_config_file(pipeline_slug: "my-org/my-pipeline")
@@ -13,6 +20,23 @@ module BuildkiteWatcher
       result = load
 
       expect(result.pipeline_slug).to eq("my-org/my-pipeline")
+    end
+
+    it "prompts the user if config file is missing" do
+      allow(prompt).to receive(:ask).and_return("my-org/my-prompted-pipeline")
+
+      result = load
+
+      expect(result.pipeline_slug).to eq("my-org/my-prompted-pipeline")
+    end
+
+    it "generates config file with user value" do
+      allow(prompt).to receive(:ask).and_return("my-org/my-prompted-pipeline")
+
+      load
+
+      expect(File.exist?(config_file_name)).to eq(true), "Expected to create file, but it was not created"
+      expect(File.read(config_file_name)).to include("pipeline_slug: my-org/my-prompted-pipeline")
     end
 
     it "loads buildkite_token from secrets file if it it exists" do
@@ -23,15 +47,6 @@ module BuildkiteWatcher
       expect(result.buildkite_token).to eq("my-super-secret-token")
     end
 
-    it "prompts the user if config file is missing" do
-      allow(prompt).to receive(:ask).and_return("my-org/my-prompted-pipeline")
-
-      load
-
-      expect(prompt).to have_received(:say)
-      expect(prompt).to have_received(:ask).with(/What's the pipeline slug of the pipeline you want to watch/)
-    end
-
     it "prompts the user if secrets file is missing"
 
     def load
@@ -39,25 +54,23 @@ module BuildkiteWatcher
     end
 
     def create_tty_config
+      puts "############ tmp_dir        ##########"
+      p tmp_dir
       config = TTY::Config.new
       config.append_path(tmp_dir)
       config
     end
 
     def create_config_file(pipeline_slug:)
-      File.write(File.join(tmp_dir, ConfigLoader::CONFIG_FILE_NAME), <<~YML)
+      File.write(config_file_name, <<~YML)
         pipeline_slug: #{pipeline_slug}
       YML
     end
 
     def create_secrets_file(buildkite_token:)
-      File.write(File.join(tmp_dir, ConfigLoader::SECRETS_FILE_NAME), <<~YML)
+      File.write(secrets_file_name, <<~YML)
         buildkite_token: #{buildkite_token}
       YML
-    end
-
-    def tty_config_double
-      double(TTY::Config, :filename= => nil, :append_path => nil, :exist? => true, :read => nil, :fetch => nil)
     end
   end
 end
